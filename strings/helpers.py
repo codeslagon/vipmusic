c8bf006347e5db654d02ec0ccf9caa094e2cb230
Lagon <109609208+codeslagon@users.noreply.github.com>
2022-10-07 23:59:28 +0300

    Create helpers.py

---
+++ strings/helpers.py
@@ -0,0 +1,41 @@
+HELP_1 = """**🎙 /play Şarkı Oynatır.**
+**🎥 /vplay Video Oynatır.**
+**📺 /stream Canlı Yayın Oynatır.**
+
+**🎞 /playlist Oynatma Listenizi Gösterir.**
+**🗑 /deleteplaylist Oynatma Listenizi Siler.**
+**🗄 /playmode Oynatma Modunu Gösterir.**
+
+**🔇 /mute Müziği Kapatır.**
+**🔉 /unmute Müziği Açar.**
+
+**📄 /sira Çalma Listesini Kontrol Ediniz.**
+
+**⚙ /auth Yetki Vermeden Komut Kullanmaya Yarar.**
+**⚙ /unauth Yetki Vermeden Komut Kullanma Yetkisini Alır.**"""
+
+
+HELP_2 = """**🎙 /play Şarkı Oynatır.**
+**🎥 /vplay Video Oynatır.**
+**📺 /stream Canlı Yayın Oynatır.**
+
+**🎞 /playlist Oynatma Listenizi Gösterir.**
+**🗑 /deleteplaylist Oynatma Listenizi Siler.**
+**🗄 /playmode Oynatma Modunu Gösterir.**."""
+
+
+HELP_3 = """**📊 /stats Grupda İstatikleri Alır.**
+**🖥 /sudolist Yetkili Kullanıcıları Gösterir.**
+**💿 /lyrics Müzik Sözlerini Gösterir.**
+**🎙 /song Müzik İndirmeye Yarar.**"""
+
+
+HELP_4 = """**✨ /start Botu Başlatır.**
+**✨ /help Yardım Menüsü.**
+**✨ /settings Grup içinde Ayarlar.**
+
+**🎛Komutlar**
+**📮 /atla Şarkı / Video Atlar.**
+**📮 /son Şarkı / Video Bitirir.**
+**📮 /durdur Şarkı / Video Devam Ettirir.**
+**📮 /devam Şarkı / Video Devam Ettirir.**"""
